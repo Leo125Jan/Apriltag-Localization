ef0e37ff6f072defbf774bdbc2345b950235e440
Leo125Jan <leomagic710@gmail.com>
2022-08-18 14:03:46 +0800

    Pose

---
+++ IMP/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/leo/IMP/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/leo/UAV/devel;/opt/ros/melodic'.split(';') if '/home/leo/UAV/devel;/opt/ros/melodic' != '' else []
---
+++ IMP/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/leo/IMP/devel:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH="/home/leo/IMP/devel/lib:$LD_LIBRARY_PATH"
+export PKG_CONFIG_PATH="/home/leo/IMP/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
+export PWD='/home/leo/IMP/build'
+export ROSLISP_PACKAGE_DIRECTORIES="/home/leo/IMP/devel/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
+export ROS_PACKAGE_PATH="/home/leo/IMP/src:$ROS_PACKAGE_PATH"
