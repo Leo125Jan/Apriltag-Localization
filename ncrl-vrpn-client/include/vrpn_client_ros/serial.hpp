9a76e1c4d15d53c7fd40384fe70323f0fa00605f
Leo125Jan <leomagic710@gmail.com>
2022-09-02 17:33:41 +0800

    d

--- ncrl-vrpn-client/include/vrpn_client_ros/serial.hpp
+++
@@ -1,8 +0,0 @@
-#ifndef __SERIAL_HPP__
-#define __SERIAL_HPP__
-
-void reg_serial_with_marker(int id, char *port_name, int baudrate);
-void send_pose_to_serial(char *tracker_name, float pos_x_cm, float pos_y_cm, float pos_z_cm,
-			 float quat_x, float quat_y, float quat_z, float quat_w);
-
-#endif
